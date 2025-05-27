af2aa2e8d22ececdd3d5b3c6eff2512ee23721ae
Panke03 <mats.panke@stud.uni-heidelberg.de>
2025-05-27 17:47:03 +0200

    Implementing the evaluation function (and knn, pca)

---
+++ functions/evaluation.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import functions.pca as pca
+import functions.knn as knn
+import functions.preprocessing as preprocessing
+
+def model_evalutaion():
+    return accurracy
--- functions/knn.py
+++ functions/knn.py
@@ -1,6 +1,7 @@
 import numpy as np
-import os
 import matplotlib.pyplot as plt
+from functions.preprocessing import train_centered, test_centered
+from functions.pca import PCA, svd_for_pca
 
 # As we will use the euclidean distance for the KNN
 # Algorithm, we will define a function to calculate 
@@ -9,4 +10,33 @@ import matplotlib.pyplot as plt
 def distance_euclidean(a,b):
     return np.square(np.sum(a - b)**2)
 
+
+def kNN_predict(train_img, test_img, k, labels):
+    # Set how many neighbours we want to consider.
+    k = 10
+
+    # Initilize an empty list to store the predictions.
+    predicitions = []
+
+    for test_img in test_img:
+
+        # Calculate the distance between the test image an all training images.
+        distance = [distance_euclidean(test_img, train_img) for train_img in train_img]
+
+        # Sort the distances and get the smallest k indices -> nearest neighbours.
+        k_indices = np.argsort(distance)[ :k]
+
+        # Get the labels of these neighbours.
+        k_labels = labels[k_indices]
+
+        # Count how often each label appears.
+        counts, values = np.unique(k_labels, return_counts = True)
+
+        # Get the label with the highest count.
+        predicted_label = values[np.argmay(counts)]
+
+        # Append the predicted label to the list.
+        predicitions.append[predicted_label]
+
+    return predicitions
 
