92fd1e50cbe990730aef21c96a980a8ce1e70e80
Panke03 <mats.panke@stud.uni-heidelberg.de>
2025-05-28 16:59:09 +0200

    Rework of the main and implementation of the evaluation

--- functions/evaluation.py
+++ functions/evaluation.py
@@ -1,8 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-import functions.pca as pca
 import functions.knn as knn
 import functions.preprocessing as preprocessing
 
-def model_evalutaion():
-    return 
+def model_evaluation(test_img, test_indices, train_img, train_labels, k):
+    """
+    test_img: Processed test images (PCA).
+    test_indices: Original incices of the test images out of the complete dateset.
+    train_img: Processed training images (PCA).
+    train_labels: Labels of the training images to assign the test images to the k nearest neighbours.
+    k: Number of nearest neighbours to consider for the KNN algorithm.
+    """
+    total_predictions = 0
+    correct_predictions = 0 
+
+    # We need the original indices (remember, the position number in the original dataset)
+    # to compare the predictions with the original labels. -> enumerate assigns an index to each image.
+    for i, test_img in enumerate(test_img):
+
+        # Using the implemented kNN algorithm to predict the label of the test images by k neighbours.
+        prediction = knn.kNN_predict(train_img, train_labels, test_img, k)
+
+        total_predictions += 1
+
+        # To get the person label 1 to 15, we need to compute the coresponding index.
+        # By dividing (true division to get an int) each test index by 11, we get the the coresponding person ID.
+        # The +1 is needed because the person IDs start at 1, not 0.
+            # Note that this method only works because we know that each person has 11 images in the dataset and
+            # we sorted them beforehand. 
+        personID = (test_indices[i] // 11) + 1 
+
+        if prediction == personID:
+
+            correct_predictions += 1
+
+        else:
+
+            continue
+
+    accuracy = correct_predictions / total_predictions
+
+    print(f"Total predictions: {total_predictions}")
+    print(f"Correct predictions: {correct_predictions}")
+    print(100 * "---")
+    print(f"Accuracy: {accuracy * 100:2f}%")
+
+    return accuracy
+
+
+
--- functions/pca.py
+++ functions/pca.py
@@ -9,28 +9,29 @@ import functions.preprocessing
 # Choosing the smallest number of k possible to capture the desired variance.
 k = 100
 
-def svd_for_pca(train_centered_svd, k):
+def svd_for_pca(X, k):
+    # X is the input data matrix (training or test set -> train_centered or test_centered).
+
     # No need for normalize the matrix as the SVD will automatically
     # calculate the direction of the highest variance.
     # U = Contains the left singular vectors (eigenfaces) -> quadratic matrix 
     # S = 1D Array of the singular values (variance) 
     # VT = Contains the right singular vectors (eigenvectors) -> quadratic matrix
-    U, S, VT = np.linalg.svd(train_centered_svd, full_matrices = False)
-
-
+    U, S, VT = np.linalg.svd(X, full_matrices = False)
     # Lets store the top k rows in a new matrix for PCA.
-    W_train = VT[ :k, : ]
+    W_dataset = VT[ :k, : ]
 
-    return W_train
+    return W_dataset, S
 
-def PCA(W_train, train_centered_svd):
+
+def PCA(W_dataset, X):
 
     # Be aware of the correct matrix mulitplication order.
     # Note that W returns as k x D matrix, where k is the number
     # of components and D the number of pixel but we need 
-    # to multiply it with the train_centered_svd matrix which
+    # to multiply it with the X matrix which
     # is N x D, where N is the number of images and D the number of pixels. 
     # -> For multiplication to work, we need to transpose W to D x k.
-    pca_train = train_centered_svd @ W_train.T
+    pca_dataset = X @ W_dataset.T
 
-    return pca_train
+    return pca_dataset
