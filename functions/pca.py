e9ac4bf1d37da8aead3208c6a442e4f95b81adbe
Fedor Belotelov <78166972+fedorbtv@users.noreply.github.com>
2025-05-29 10:23:50 +0200

    Added PCA

--- functions/pca.py
+++ functions/pca.py
@@ -3,20 +3,22 @@ import os
 import numpy as np
 from PIL import Image
 
-def  pca(X, n_components):
-    # X is the input data matrix (training or test set -> train_centered or test_centered).
+# Function to perform PCA on a dataset of images
+# X is the input data matrix, n_components is the number of principal components to keep
+def  pca(dat_matrix, n_components):
+    
     # No need for normalize the matrix as the SVD will automatically
     # calculate the direction of the highest variance.
     # U = Contains the left singular vectors (eigenfaces) -> quadratic matrix 
     # S = 1D Array of the singular values (variance) 
     # VT = Contains the right singular vectors (eigenvectors) -> quadratic matrix
-    #performing SVD
+    # performing SVD
     #we use full_matrices=False to get reduced matrices
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    U, S, VT = np.linalg.svd(dat_matrix, full_matrices=False)
     #selecting the first n_components
-    eigenfaces = U[:, :n_components]
+    U_reduced = U[:, :n_components]
     S_reduced = S[:n_components]
-    V_reduced  = Vt[:n_components, :]
+    V_reduced  = VT[:n_components, :]
 
     # Be aware of the correct matrix mulitplication order.
     # Note that W returns as k x D matrix, where k is the number
@@ -26,9 +28,13 @@ def  pca(X, n_components):
     # -> For multiplication to work, we need to transpose W to D x k.
     
     #projecting the data onto the reduced space 
-    X_reduced = X @ V_reduced.T
+    dat_matrix_reduced = dat_matrix @ V_reduced.T
 
     #defining the eigenvalues 
     eigenvalues = S_reduced ** 2
     #returning reduced data
-    return eigenfaces, S_reduced, V_reduced, X_reduced, eigenvalues
+
+    print(f"Original Data Shape: {dat_matrix.shape}")
+    print(f"Reduced Data Shape: {dat_matrix_reduced.shape}\n")
+
+    return U_reduced, S_reduced, V_reduced, dat_matrix_reduced, eigenvalues
