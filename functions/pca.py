078b13379da5d4c0ae2d2c8b573959e1a75dc085
Panke03 <mats.panke@stud.uni-heidelberg.de>
2025-06-19 10:54:23 +0200

    Renaming

--- functions/pca.py
+++ functions/pca.py
@@ -1,137 +1,74 @@
-# Here all the preprocessing steps are defined including splitting the dataset
+# PCA (Principal Component Analysis) implementation
 import os
 import numpy as np
 from PIL import Image
 
-def preprocessing(data_path, seed, train_ratio= 0.7, verbose=True):
-    """Preprocesses image data from the specified directory and splits it into training and testing sets.
-
-    Each GIF image in the given directory is processed as follows:
-      - Only files with the .gif extension are considered.
-      - The subject's identifier is extracted from the filename (the portion before the first dot).
-      - The image is opened, converted to grayscale ('L' mode), normalized (dividing pixel values by 255),
-        and then flattened into a 1D NumPy array.
-    
-    Images for each subject are grouped together, then shuffled and split into training and testing sets.
-    For each subject, the first 8 images (after shuffling) are used for training and the next 3 for testing.
-    Finally, both training and testing images are standardized using the training set's mean 
-    and standard deviation.
+# Function to perform PCA on a dataset of images
+def svd_pca(input_matrix, n_components, verbose=True):
+    """Compute the PCA transformation for the training set dat_matrix using SVD.
 
     Args:
-        data_path (str): The path to the directory containing the image files.
-        seed():
-        train_ratio():
+        input_matrix (ndarray of shape (n_samples, n_features)):
+            Training data.
+        n_components (int) : 
+            Number of principal components to keep.
 
     Returns:
-        final_train (numpy.ndarray): 
-            The training data, standardized, with shape (n_train, num_features).
-        final_test (numpy.ndarray):
-            The testing data, standardized using the training set's parameters.
-        train_labels (list):
-            A list of subject identifiers corresponding to each training image.
-        test_labels (list):
-            A list of subject identifiers corresponding to each testing image.
-        test_arr (numpy.ndarray):
-            The original (non-standardized) testing data as a NumPy array.
+        projection_matrix (ndarray of shape (n_components, n_features)) : 
+            The PCA projection matrix.
+        train_reduced (ndarray of shape (n_samples, n_components)) : 
+            The reduced training data in the PCA space.
+        explained_variance_ratio (ndarray of shape (n_components,)) : 
+            The variance explained by each principal component.
     """
+    # VT = Contains the right singular vectors (eigenvectors) -> rectangular matrix (A^TA)^T	
+    # U = Contains the left singular vectors (eigenvectors) -> rectangular matrix (AA^T)
+    # we use full_matrices=False to get reduced matrices
+    U, S, VT = np.linalg.svd(input_matrix, full_matrices=False)
 
-    # Create a dictionary to group images by individual.
-    grouped_images = {}
+    # S contains the singular values, which are the square roots of the eigenvalues and lie on the diagonal of matrix sigma
+    # We take the first n_components singular values and corresponding vectors (projection matrix)
+    singular_values = S[:n_components]
+    projection_matrix  = VT[:n_components, :]
 
-    # Load images and group them by individual.
-    for img_file in sorted(os.listdir(data_path)):
-        # Check if the file is a GIF image, if not, it will be skipped.
-        if not img_file.endswith(".gif"):
-            continue
+    # Be aware of the correct matrix mulitplication order.
+    # Note that projection_matrix returns as n x D matrix, where n is the number
+    # of components and D the number of pixel but we need to multiply it with the input_data which
+    # is N x D, where N is the number of images and D the number of pixels. 
+    # -> For multiplication to work, we need to transpose projection_matrix to D x n.
+    
+    # projecting the data onto the reduced space 
+    train_reduced = input_matrix @ projection_matrix.T
 
-        # Extract person’s identifier from the filename.
-        # I our case, the identifier is the first part of the filename before the dot.
-        # [0] splits the string at the dot and takes the first part.
-        subject_id = img_file.split(".")[0]
-        
-        # Open the image file and convert it to grayscale.
-        img_path = os.path.join(data_path, img_file)
-        with Image.open(img_path) as img:
-            # The 'L' mode is for grayscale images.
-            img = img.convert("L")
-            # Convert the grayscale image to a NumPy array for numerical processing
-            # Convert to a float32 NumPy array (this converts the integer data into float)
-            # then normalize by dividing by 255 so that pixel values are in [0,1]
-            image_data = np.array(img, dtype=np.float32) / 255.0
-            # Flatten the 2D image array into a 1D vector.
-            flat_image = image_data.flatten()
-        
-        # Check if the subject_id is already in the dictionary.
-        # If not, create a new list for that subject.
-        # If it is, append the image data to the existing list.
-        # This way, we group all images of the same subject together.
-        if subject_id not in grouped_images:
-            grouped_images[subject_id] = []
-        grouped_images[subject_id].append(flat_image)  
+    # defining the eigenvalues 
+    eigenvalues = singular_values ** 2
+    # calculating the variance explained by each component
+    # we need to divide the eigenvalues by the total sum of eigenvalues to get the explained variance ratio
+    # we convert the absolute eigenvalues to relative values
+    explained_variance_ratio = eigenvalues / np.sum(eigenvalues)
+    # returning reduced data
+    if verbose:
+        print(f"\nSuccesfully reduced Matrix from {input_matrix.shape} to {train_reduced.shape}\n")
 
-    # Set a random seed for reproducibility (output is the same every time).
-    np.random.seed(seed)
+    return projection_matrix, train_reduced, explained_variance_ratio
 
-    # Prepare separate lists for training and testing images.
-    train_data = []
-    test_data = []
-    # Prepare labels lists, which will later be used for the KNN classifier.
-    train_labels = []
-    test_labels = []
+def pca_transform(test_data, projection_matrix, verbose=True):
+    """Transform the data matrix using the PCA projection matrix.
 
-    # For each individual, randomly assign 8 images to training and 3 to testing.
-    for subject, images in grouped_images.items():
-        # Convert list of images to a numpy array for shuffling. 
-        # difference between list and numpay array is that numpy arrays are more optimized for numerical operations.
-        images = np.array(images)
-        
-        # Shuffle images in-place with NumPy's shuffle.
-        # This shuffling is along axis 0, meaning it shuffles the rows (images) randomly within the array.
-        np.random.shuffle(images)
+    Args:
+        test_data (ndarray of shape (n_samples, n_features)): 
+            The data to be transformed.
+        projection_matrix (ndarray of shape (n_components, n_features)):
+            The PCA projection matrix.
 
-        # Calculate the split index based on the train_ratio
-        split_index = int(len(images) * train_ratio)
-        
-        # Split into training and testing images
-        subject_train = images[:split_index]
-        subject_test = images[split_index:]
-
-        # Append images and corresponding labels
-        train_data.extend(subject_train)
-        # Multiplying [subject] by len(subject_train) creates a list where the same subject ID repeats n times 
-        # (where n is the number of training images for that subject).
-        # This is used for the KNN classifier later.
-        train_labels.extend([subject] * len(subject_train)) 
-        #.extend is used to add elements of the list subject_train to train_data individually.
-        #the output is one list, instead of a list of lists.(this would have been the case if we used .append)
-        test_data.extend(subject_test)
-        test_labels.extend([subject] * len(subject_test))
+    Returns:
+        test_reduced (ndarray of shape (n_samples, n_components)) : 
+            The transformed data in the PCA space.
+    """       
+    # Project the test data onto the PCA space using the projection matrix V_reduced
+    test_reduced = test_data @ projection_matrix.T
 
     if verbose:
-        # output you see in main.ipynb
-        print(f"Total training images: {len(train_data)}")
-        print(f"Total testing images: {len(test_data)}")
+        print(f"Succesfully transformed Matrix from {test_data.shape} to {test_reduced.shape}")
 
-
-    #  Now Normalization & Standardization is performed
-
-    # Convert lists to NumPy arrays.
-    train_arr = np.array(train_data)  # Shape: (n_train, num_features)
-    test_arr = np.array(test_data)   # Shape: (n_test, num_features)
-
-    # Compute global mean and standard deviation from training data only.
-    # axis = 0 means we compute the mean and std across all training images for each feature (pixel).
-    train_mean = np.mean(train_arr, axis=0)
-    global_std  = np.std(train_arr, axis=0)
-
-    # To avoid division by zero, replace any zeros in the std vector.
-    # np.where checks where global_std is zero and replaces it with a small value (1e-8).
-    global_std = np.where(global_std == 0, 1e-8, global_std)
-
-    # Center the training set.
-    final_train = (train_arr - train_mean) / global_std
-
-    # Apply the same transformation to the test set.
-    final_test = (test_arr - train_mean) / global_std
-
-    return final_train, train_labels, final_test, test_labels, test_arr
+    return test_reduced
